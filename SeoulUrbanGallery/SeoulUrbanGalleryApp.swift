6e1d308146502edf8cc828dfc20e29e616b0f79c
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-07-03 13:22:19 +0900

    Initial Commit

---
+++ SeoulUrbanGallery/SeoulUrbanGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeoulUrbanGalleryApp.swift
+//  SeoulUrbanGallery
+//
+//  Created by Jinwook Kim on 2023/07/03.
+//
+
+import SwiftUI
+
+@main
+struct SeoulUrbanGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
