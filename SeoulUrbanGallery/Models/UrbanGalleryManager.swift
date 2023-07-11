2da25193e708b556f1f35594518b2d45955765c3
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-07-11 14:24:02 +0900

    Add the custom urban gallery saving functionality.

--- SeoulUrbanGallery/Models/UrbanGalleryManager.swift
+++ SeoulUrbanGallery/Models/UrbanGalleryManager.swift
@@ -22,6 +22,36 @@ import SwiftUI
         let endPointString = "http://openapi.seoul.go.kr:8088/49497a51466c696e33355546774f7a/json/gongGongArtGallery/1/1000"
         return URL(string: endPointString)!
     }
+    private func makeCustomDirectory() {
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let customDirectory = documentDirectory.appendingPathComponent("Custom")
+        if !FileManager.default.fileExists(atPath: customDirectory.path) {
+            do {
+                try FileManager.default.createDirectory(atPath: customDirectory.path, withIntermediateDirectories: true)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    private func loadCustomData() -> [UrbanGallery] {
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let customDirectory = documentDirectory.appendingPathComponent("Custom")
+        makeCustomDirectory()
+        guard let customURLs = try? FileManager.default.contentsOfDirectory(at: customDirectory, includingPropertiesForKeys: nil) else {
+            fatalError("Failed to load custom directory.")
+        }
+        var customUrbanGallery: [UrbanGallery] = []
+        for url in customURLs {
+            guard let data = try? Data(contentsOf: url) else {
+                fatalError("Failed to load custom data.")
+            }
+            guard let decodedData = try? JSONDecoder().decode(UrbanGallery.self, from: data) else {
+                fatalError("Failed to decode custom data.")
+            }
+            customUrbanGallery.append(decodedData)
+        }
+        return customUrbanGallery
+    }
     func loadData() async {
         guard let (data, _) = try? await URLSession.shared.data(from: endPoint) else {
             fatalError("Failed to load data.")
@@ -29,7 +59,8 @@ import SwiftUI
         guard let decodedData = try? JSONDecoder().decode(UrbanGalleryHeader.self, from: data) else {
             fatalError("Failed to decode data.")
         }
-        urbanGallery = decodedData.header.rows
+        let customs = loadCustomData()
+        urbanGallery = decodedData.header.rows + customs
         count = decodedData.header.count
     }
     func searchedUrbanGallery(with searchText: String) -> [UrbanGallery] {
@@ -42,6 +73,15 @@ import SwiftUI
         }
     }
     func add(_ customUrbanGallery: UrbanGallery) {
+        save(customUrbanGallery)
         urbanGallery.append(customUrbanGallery)
+    }
+    private func save(_ customUrbanGallery: UrbanGallery) {
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let targetURL = documentDirectory.appendingPathComponent("Custom/\(customUrbanGallery.id)")
+        guard let encodedUrbanGallery = try? JSONEncoder().encode(customUrbanGallery) else {
+            fatalError("Failed to encode custom urban gallery.")
+        }
+        try? encodedUrbanGallery.write(to: targetURL, options: [.atomic, .completeFileProtection])
     }
 }
